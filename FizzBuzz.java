eaad1f88027e99c8c57de4b1af599bb0552efdf4
aamnahmalik <aamnah@icloud.com>
2023-03-27 17:44:21 -0400

    created fizz buzz algorithm

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,6 +4,25 @@ public class FizzBuzz
 {
     public static void main(String[] args)
     {
-        
+        for(int i = 1; i <= 100; i++)
+        {
+            int currentNum = i;
+            if((currentNum % 3 == 0) && (currentNum % 5 == 0))
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if(currentNum % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(currentNum % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(currentNum);
+            }
+        }
     }
 }
